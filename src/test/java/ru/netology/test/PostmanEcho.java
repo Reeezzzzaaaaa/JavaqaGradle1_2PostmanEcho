2acf6da796fd79cebea877ce90d26475bee94864
АлександÑAleksandr Ryazantsev <amapkymap@gmail.com>
2022-08-19 18:59:49 +0300

    Postman Echo commit

---
+++ src/test/java/ru/netology/test/PostmanEcho.java
@@ -0,0 +1,48 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEcho {
+
+    @Test
+    void shouldSomeDataTest() {
+        // Given - When - Then
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+
+    @Test
+    void shouldTypeTest() {
+        // Given - When - Then
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("type", equalTo(null));
+    }
+
+    @Test
+    void shouldCurrencyTest() {
+        // Given - When - Then
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("currency", equalTo(null));
+    }
+}
